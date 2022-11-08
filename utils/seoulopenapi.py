41c5b2d685f3f2714ade6cf7e83735dd92623896
xxddongxx <hyeon2187@gmail.com>
2022-11-08 22:49:56 +0900

    FEAT: SeoulOpenApi Class 생성

---
+++ utils/seoulopenapi.py
@@ -0,0 +1,9 @@
+class SeoulOpenApi:
+    def __init__(self):
+        self.host = "http://openapi.seoul.go.kr:8088/"
+        self.key = "765076694b68796539307869467670/"
+        self.function_name = ""
+        self.type = "json/"
+        self.start = "1/"
+        self.end = "1000/"
+        self.gu_name = ""
