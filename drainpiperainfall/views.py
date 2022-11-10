4c0455c858cd7e2c869583f5350633403d2e6463
xxddongxx <hyeon2187@gmail.com>
2022-11-10 22:20:39 +0900

    FEAT: 하수관 수위 오늘 데이터 조회 api

--- drainpiperainfall/views.py
+++ drainpiperainfall/views.py
@@ -11,7 +11,7 @@ from utils.rainfallcontroller import RainFallController
 class Least(APIView):
     def get(self, request):
         """
-        GET /api/v1/drainpiperainfall/latest/
+        GET /api/v1/drainpiperainfall/latest/?gu_name="용산구" or "용산"
         """
         gu_name = request.GET["gu_name"]
         try:
@@ -52,11 +52,11 @@ class Least(APIView):
             )
 
 
-class Today(APIView):
+class RainFallToday(APIView):
     def get(self, request):
         """
         오늘 데이터 조회
-        GET /api/v1/drainpiperainfall/rainfall/today/
+        GET /api/v1/drainpiperainfall/rainfall/today/?gu_name="용산구" or "용산"
         """
         gu_name = request.GET["gu_name"]
         try:
@@ -64,6 +64,7 @@ class Today(APIView):
             rainfall_url = rainfall.get_url()
             datas, set_len = rainfall.get_datas_set_len(rainfall_url)
             rainfall_result = rainfall.get_today_result(datas, set_len)
+            rainfall_total_count = len(rainfall_result)
 
             rainfall_serializer = serializers.RainFallSerializer(
                 rainfall_result, many=True
@@ -74,7 +75,47 @@ class Today(APIView):
                     "message": "Success",
                     "status": status.HTTP_200_OK,
                     "result": {
+                        "now_total_count": rainfall_total_count,
                         "RainFall": rainfall_serializer.data,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as error:
+            return Response(
+                {
+                    "message": "데이터를 찾을 수 없습니다.",
+                    "status": status.HTTP_404_NOT_FOUND,
+                    "result": {},
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+class DrainPipeToday(APIView):
+    def get(self, request):
+        """
+        오늘 데이터 조회
+        GET /api/v1/drainpiperainfall/drainpipe/today/?gu_name="용산구" or "용산"
+        """
+        gu_name = request.GET["gu_name"]
+        try:
+            drain = DrainPipeController(gu_name)
+            drain_url = drain.get_latest_url()
+            drain_today_url_list = drain.make_today_url_list(drain_url)
+            drain_today_result = drain.thread_executor_method(drain_today_url_list)
+            drain_total_count = len(drain_today_result)
+
+            drain_serializer = serializers.DrainPipeSerializer(
+                drain_today_result, many=True
+            )
+            return Response(
+                {
+                    "message": "Success",
+                    "status": status.HTTP_200_OK,
+                    "result": {
+                        "now_total_count": drain_total_count,
+                        "Drain_Pipe": drain_serializer.data,
                     },
                 },
                 status=status.HTTP_200_OK,
