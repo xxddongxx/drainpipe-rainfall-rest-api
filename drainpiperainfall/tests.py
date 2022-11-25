abde7fea80a757e3f41b22871daac9b34ae9e470
xxddongxx <hyeon2187@gmail.com>
2022-11-25 15:43:33 +0900

    TEST: 현 시간까지의 가우량 데이터 조회 API 성공 테스트

--- drainpiperainfall/tests.py
+++ drainpiperainfall/tests.py
@@ -36,3 +36,19 @@ class TestDrainPipeRainfallAPI(APITestCase):
         self.assertIn("message", response_data)
         self.assertIn("status", response_data)
         self.assertIn("result", response_data)
+
+    def test_rainfall_today_success(self):
+        """
+        현 시간까지의 강우량 데이터 조회 성공 테스트
+        """
+        rainfall_today_url = f"{PATH}rainfall/today/?gu_name={self.gu_name}&page=1"
+        response = self.client.get(rainfall_today_url)
+        response_data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response_data, dict)
+        self.assertIn("message", response_data)
+        self.assertIn("status", response_data)
+        self.assertIn("total_count", response_data)
+        self.assertIn("total_page", response_data)
+        self.assertIn("result", response_data)
