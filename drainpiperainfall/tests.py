7ecf78a7de0672f5175388bac1b25e98e85aa2dc
xxddongxx <hyeon2187@gmail.com>
2022-11-25 15:38:36 +0900

    TEST: 최신 데이터 조회 API 성공 테스트

--- drainpiperainfall/tests.py
+++ drainpiperainfall/tests.py
@@ -22,3 +22,17 @@ class TestDrainPipeRainfallAPI(APITestCase):
         self.assertIn("message", response_data)
         self.assertIn("status", response_data)
         self.assertIn("result", response_data)
+
+    def test_latest_api_success(self):
+        """
+        최신 데이터 조회 테스트
+        """
+        latest_url = f"{PATH}latest/?gu_name={self.gu_name}"
+        response = self.client.get(latest_url)
+        response_data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response_data, dict)
+        self.assertIn("message", response_data)
+        self.assertIn("status", response_data)
+        self.assertIn("result", response_data)
